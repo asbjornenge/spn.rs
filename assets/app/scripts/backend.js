94537344fd8d9a851aaba15aaef3a71a805c6699
Asbjørn Enge <asbjorn@hanafjedle.net>
2013-12-29 19:28:43 +0100

    Started work

---
+++ assets/app/scripts/backend.js
@@ -0,0 +1,32 @@
+define(['radio','fb/firebase','fb/firebase-simple-login'],function(radio) {
+
+    function backend() {
+        this.auth = null;
+        this.root = new Firebase('https://spnrs.firebaseio.com/');
+    }
+    backend.prototype.init = function() {
+        this.auth = new FirebaseSimpleLogin(this.root, function(error, user) {
+            radio('user.login').broadcast(error ? error : user ? user : null)
+        });
+        return this;
+    }
+    backend.prototype.login = function(provider) {
+        var options = {}
+        switch(provider) {
+            case 'github':
+                options = { rememberMe : true, scope : 'user'}
+                break;
+            default:
+                console.log('Unknown provider '+provider); return;
+        }
+        this.auth.login(provider, options)
+        return this;
+    }
+    backend.prototype.logout = function() {
+        auth.logout();
+        return this;
+    }
+
+    return new backend();
+
+})
--- assets/app/scripts/main.js
+++ assets/app/scripts/main.js
@@ -1,9 +1,17 @@
 require.config({
-    baseUrl : '/scripts',
-    paths: {}
+    paths: {
+        fb    : '../bower_components/firebase/lib',
+        radio : '../bower_components/Radio/radio'
+    }
 });
 
-require([], function() {
-    console.log('application ready')
+require(['radio','backend'], function(radio, backend) {
+    /* SETUP LISTENERS */
+    radio('user.login').subscribe(function(user) {
+        console.log('user', user);
+    })
+
+    /* INITIALIZED */
+    backend.init();
 });
 
