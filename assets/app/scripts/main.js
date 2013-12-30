4eb769bf7af2c07385a0236bd7882bf36e9847b1
Asbjørn Enge <asbjorn@hanafjedle.net>
2013-12-30 10:14:45 +0100

    Added nanodom

--- assets/app/scripts/main.js
+++ assets/app/scripts/main.js
@@ -1,11 +1,14 @@
 require.config({
     paths: {
-        fb    : '../bower_components/firebase/lib',
-        radio : '../bower_components/Radio/radio'
+        fb      : '../bower_components/firebase/lib',
+        radio   : '../bower_components/Radio/radio',
+        nanodom : '../bower_components/nanodom/nanodom'
     }
 });
 
-require(['radio','backend'], function(radio, backend) {
+// TODO : Have a html splash screen while checking login
+
+require(['radio','backend','nanodom'], function(radio, backend, dom) {
     /* SETUP LISTENERS */
     radio('user.login').subscribe(function(user) {
         console.log('user', user);
