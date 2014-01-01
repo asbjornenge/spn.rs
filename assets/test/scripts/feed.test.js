7124c0e45b40d426025e590dd26ad000cd153af8
Asbjørn Enge <asbjorn@hanafjedle.net>
2014-01-01 21:40:17 +0100

    Added Feed and tests

---
+++ assets/test/scripts/feed.test.js
@@ -0,0 +1,29 @@
+define(['chai','scripts/feed','radio'], function(chai, feed, radio) {
+
+    describe('FEED', function() {
+
+        var assert = chai.assert;
+
+        it('Should call registered .on functions', function(done) {
+            feed('global').on('new', function() {assert.ok(true); done()})
+            radio('feed.global.new').broadcast('new spnr');
+        })
+
+        it('Should remove registered functions on .off', function() {
+            var callback = sinon.spy();
+            feed('global').on('new', callback).off('new', callback)
+            radio('feed.global.new').broadcast('new spnr');
+            assert.ok(!callback.calledOnce)
+        })
+
+        it('Should return an array for .all calls', function(done) {
+            feed('global').all(function(data) { assert.ok(data instanceof Array); done() })
+        })
+
+        it('Should return an array for .take calls', function(done) {
+            feed('global').take(5,function(data) { assert.ok(data instanceof Array); done() })
+        })
+
+    })
+
+})
--- assets/test/specRunner.js
+++ assets/test/specRunner.js
@@ -1,13 +1,19 @@
 require.config({
     baseUrl : '../app/',
     paths: {
-        chai   : 'bower_components/chai/chai'
+        chai    : 'bower_components/chai/chai',
+        fb      : 'bower_components/firebase/lib',
+        radio   : 'bower_components/Radio/radio',
+        react   : 'bower_components/react/react',
+        nanodom : 'bower_components/nanodom/nanodom',
+        comp    : 'components'
+
     },
     urlArgs: "v="+(new Date()).getTime()
 });
 
 require([
-    '../test/spec.js',
+    '../test/scripts/feed.test.js',
 ],
 function() {
     mocha.run()
