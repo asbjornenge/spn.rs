89d2b1c21cbbc57a17c5d05fb48ff7f291bf0443
Asbjørn Enge <asbjorn@hanafjedle.net>
2014-05-13 21:58:19 +0200

    Fixed global leak

--- modules/components/SpnrList.js
+++ modules/components/SpnrList.js
@@ -3,7 +3,7 @@ var SpnrListItem = require('./SpnrListItem')
 
 var SpnrList = React.createClass({
     render : function() {
-        SpnrListItems = this.props.state[this.props.state.view].map(function(spnr) {
+        var SpnrListItems = this.props.state[this.props.state.view].map(function(spnr) {
             return SpnrListItem({spnr : spnr, state : this.props.state, emitter : this.props.emitter})
         }.bind(this))
         return (
