e1b0ad9bcc64c5b98efce34a7d5bf67894e1b834
Asbjørn Enge <asbjorn@hanafjedle.net>
2014-05-13 22:10:12 +0200

    Added a common mock state

---
+++ test/mock.js
@@ -0,0 +1,33 @@
+module.exports = {
+    view      : 'global',
+    global    : [
+        {
+            spnr : 'Rene pumper',
+            user : 1
+        }
+    ],
+    mine      : [
+        {
+            spnr : 'Rene pumper',
+            user : 1
+        }
+    ],
+    favorites : [],
+    current   : {
+        spnr : 'Rene pumper',
+        user : 1
+    },
+    user      : {
+        uid : 1
+    },
+    avatars   : {},
+    latest    : {
+        global : {
+            loaded : null,
+            seen   : null
+        },
+        mine : {
+            loaded : null
+        }
+    }
+}
